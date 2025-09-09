4328e1d6504e49436a75adfc5ce0044ec3ee15a1
BoutrosMessiha2002 <125684656+BoutrosMessiha2002@users.noreply.github.com>
2025-09-10 01:01:05 +0300

    Update routing.tcl

--- pnr/routing/routing.tcl
+++ pnr/routing/routing.tcl
@@ -5,24 +5,10 @@ open_block $design_lib_path/${design}.dlib:${design}_placement.design
 copy_block -from_block ${design}.dlib:${design}_placement.design -to_block ${design}_routing
 current_block ${design}_routing.design
 
-
-report_qor -summary
-check_design -checks pre_route_stage
-
 global_route_opt
 route_global
-
-check_pg_drc
-
-
 route_track
-
-check_pg_drc
-
 route_detail
-
-check_pg_drc
-
 check_routes
 
 
@@ -35,40 +21,13 @@ check_pg_drc
 check_routes -drc true
 
 remove_stdcell_fillers_with_violation
-
 check_legality
-
-
-
 write_verilog  ./${design}_routing_Netlist.v
-
-
 write_sdc -output ./${design}_routing_SDC.sdc
-
-
 write_parasitics -format SPEF -output ./${design}_Routing_Spef.spef
-
-
 write_def ../output/${design}_Routing_def.def
 
-
-
-
-write_gds \
--view design \
--lib_cell_view frame \
--output_pin all \
--fill include \
--exclude_empty_block \
--long_names \
--keep_data_type \
-../GDS/${design}_Routing_GDS.gds
-
-
-
-
-
-
+write_gds -view design -lib_cell_view frame -output_pin all -fill include -exclude_empty_block -long_names -keep_data_type ../GDS/${design}_Routing_GDS.gds
 
 save_block
 
